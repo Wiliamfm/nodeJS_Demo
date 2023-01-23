320e095244edc0adda00e541beb54cde0d1921d2
William Fonseca <santifon15@gmail.com>
2023-01-23 18:43:12 -0500

    remove authentication validation

--- middlewares/auth.validation.middleware.js
+++ middlewares/auth.validation.middleware.js
@@ -1,9 +1,12 @@
 function isAuthenticated(req, res, next) {
+   return next();
+   /*
    if (req.session.user) {
       return next();
    }
    req.session.error = "Not authenticated";
    return res.status(401).send(req.session.error);
+   */
 }
 
 export default isAuthenticated;
